9c1f1aedd842decc427ebf2e0e7d404f41c37250
chenyiqun <53168756+chenyiqun@users.noreply.github.com>
2024-01-24 19:41:13 +0800

    Update episode_runner.py

--- runners/episode_runner.py
+++ runners/episode_runner.py
@@ -15,53 +15,17 @@ class EpisodeRunner:
 
         if self.args.env_args['map_name'] == 'search_engine':
 
-            # fileQueryPermutaion = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/10000/query_permutation.json'
-            # fileQueryDocumentRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/10000/query_doc_features.json'
-            # fileQueryDocumentSubtopics = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/10000/query_doc_subtopics.json'
+            fileQueryPermutaion = './data/baidu/doc15_2/query_permutation_doc15_2.json'
+            fileQueryRepresentation = './data/baidu/doc15_2/query_features_doc15_2_acge.json'
+            fileDocumentRepresentation = './data/baidu/doc15_2/doc_features_doc15_2_acge.json'
+            fileQueryDocumentSubtopics = './data/baidu/doc15_2/query_doc_subtopics_doc15_2.json'
 
-            # fileQueryPermutaion = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/300000/query_permutation.json'
-            # fileQueryDocumentRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/300000/query_doc_features.json'
-            # fileQueryDocumentSubtopics = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/300000/query_doc_subtopics.json'
-
-            # fileQueryPermutaion = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/10000_20/query_permutation.json'
-            # fileQueryDocumentRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/10000_20/query_doc_features.json'
-            # fileQueryDocumentSubtopics = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/10000_20/query_doc_subtopics.json'
-
-            # fileQueryPermutaion = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/300000_20/query_permutation.json'
-            # fileQueryDocumentRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/300000_20/query_doc_features.json'
-            # fileQueryDocumentSubtopics = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/300000_20/query_doc_subtopics.json'
-
-            # fileQueryPermutaion = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15/210000/query_permutation_210000.json'
-            # fileQueryDocumentRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15/210000/query_doc_features_210000.json'
-            # fileQueryDocumentSubtopics = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15/210000/query_doc_subtopics_210000.json'
-
-            # fileQueryPermutaion = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15/1000001_1356074/query_permutation_1000001_1356074.json'
-            # fileQueryDocumentRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15/1000001_1356074/query_doc_features_1000001_1356074.json'
-            # fileQueryDocumentSubtopics = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15/1000001_1356074/query_doc_subtopics_1000001_1356074.json'
-
-            # fileQueryPermutaion = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_2/query_permutation_doc15_2.json'
-            # fileQueryDocumentRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_2/query_doc_features_doc15_2_acge.json'
-            # fileQueryDocumentSubtopics = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_2/query_doc_subtopics_doc15_2.json'
-
-            # fileQueryPermutaion = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_2/query_permutation_doc15_2.json'
-            # fileQueryRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_2/query_features_doc15_2_acge.json'
-            # fileDocumentRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_2/doc_features_doc15_2_acge.json'
-            # fileQueryDocumentSubtopics = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_2/query_doc_subtopics_doc15_2.json'
-            
-            fileQueryPermutaion = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_1_2/query_permutation_doc15_all.json'
-            fileQueryRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_1_2/query_features_doc15_2_acge.json'
-            fileDocumentRepresentation = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_1_2/doc_features_doc15_2_acge.json'
-            fileQueryDocumentSubtopics = '/home/chenyiqun/code/pymarl2-master-baidu/src/data/baidu/doc15_1_2/query_doc_subtopics_doc15_all.json'
-
-            # 读取数据
+            # load data
             with open(fileQueryPermutaion) as self.fileQueryPermutaion:
                 self.dictQueryPermutaion = json.load(self.fileQueryPermutaion)
 
             with open(fileQueryRepresentation) as self.fileQueryRepresentation:
                 self.dictQueryRepresentation = json.load(self.fileQueryRepresentation)
-            # for query in self.dictQueryRepresentation:
-            #     self.dictQueryRepresentation[query] = np.matrix([self.dictQueryRepresentation[query]], dtype=float)
-            #     self.dictQueryRepresentation[query] = np.transpose(self.dictQueryRepresentation[query])  # 转换成100*1的矩阵
             
             with open(fileDocumentRepresentation) as self.fileDocumentRepresentation:
                 self.dictDocumentRepresentation = json.load(self.fileDocumentRepresentation)
@@ -104,23 +68,12 @@ class EpisodeRunner:
 
     def reset(self, query_id, doc_list, obs_actions, test_mode):
         self.t = 0
-        self.batch = self.new_batch()  # 创建保存本次episode的batch，是一个非常完整的数据结构，由scheme等信息创建。
+        self.batch = self.new_batch()  
         self.env.reset(query_id, doc_list, obs_actions, test_mode, self.t)
-        # self.t = 0
 
-    def run(self, test_mode=False, query_id='', doc_list=[]):  # 此处还需结合搜索的特点进行改动。
+    def run(self, test_mode=False, query_id='', doc_list=[]): 
 
-        # # 获取所有doc的特征
-        # c = []
-        # c_id = []
-        # for can in doc_list:
-        #     # if can not in listSelectedSet_without_mcts:
-        #     doc_repr = self.env.dictDocumentRepresentation[can]
-        #     doc_repr = np.reshape(np.asarray(doc_repr), -1).tolist()
-        #     c.append(doc_repr)
-        #     c_id.append(can)
-
-        # 初始化动作分数
+        # initial scores
         obs_actions = [0] * self.env.n_agents
         obs_actions = self.get_onehot(obs_actions)
 
@@ -131,6 +84,7 @@ class EpisodeRunner:
         episode_return_list = []
         alpha_NDCG_5_list, alpha_NDCG_10_list = [], []
         ERR_IA_5_list, ERR_IA_10_list = [], []
+        S_recall_5_list, S_recall_10_list = [], []
         self.mac.init_hidden(batch_size=self.batch_size)
 
         while not terminated:
@@ -150,7 +104,7 @@ class EpisodeRunner:
             cpu_actions = actions.to("cpu").numpy()
             
             if test_mode:
-                alpha_NDCG_5, alpha_NDCG_10, ERR_IA_5, ERR_IA_10, reward, terminated, env_info, cur_ranks = self.env.step(actions[0], query_id, doc_list, t=self.t, test_mode=test_mode, sparse_reward=self.args.sparse_reward)
+                alpha_NDCG_5, alpha_NDCG_10, ERR_IA_5, ERR_IA_10, S_recall_5, S_recall_10, reward, terminated, env_info, cur_ranks = self.env.step(actions[0], query_id, doc_list, t=self.t, test_mode=test_mode, sparse_reward=self.args.sparse_reward)
             else:
                 reward, terminated, env_info, cur_ranks = self.env.step(actions[0], query_id, doc_list, t=self.t, test_mode=test_mode, sparse_reward=self.args.sparse_reward)
             episode_return += reward
@@ -160,6 +114,8 @@ class EpisodeRunner:
                 alpha_NDCG_10_list.append(alpha_NDCG_10)
                 ERR_IA_5_list.append(ERR_IA_5)
                 ERR_IA_10_list.append(ERR_IA_10)
+                S_recall_5_list.append(S_recall_5)
+                S_recall_10_list.append(S_recall_10)
 
             post_transition_data = {
                 "actions": cpu_actions,
@@ -217,7 +173,8 @@ class EpisodeRunner:
             # self.logger.log_stat("test_alpha_ndcg_5_by_step", alpha_NDCG_5_list, self.t_env)
             # self.logger.log_stat("test_alpha_ndcg_10_by_step", alpha_NDCG_10_list, self.t_env)
 
-            return episode_return_list[-1], alpha_NDCG_5_list[-1], alpha_NDCG_10_list[-1], ERR_IA_5_list[-1], ERR_IA_10_list[-1]
+            # return episode_return_list[-1], alpha_NDCG_5_list[-1], alpha_NDCG_10_list[-1], ERR_IA_5_list[-1], ERR_IA_10_list[-1]
+            return episode_return_list[-1], alpha_NDCG_5_list[-1], alpha_NDCG_10_list[-1], ERR_IA_5_list[-1], ERR_IA_10_list[-1], S_recall_5_list[-1], S_recall_10_list[-1]
             # return episode_return_list[0], alpha_NDCG_5_list[0], alpha_NDCG_10_list[0], ERR_IA_5_list[0], ERR_IA_10_list[0]
 
             # return episode_return_list[0], alpha_NDCG_5_list[0], alpha_NDCG_10_list[0]
@@ -236,16 +193,12 @@ class EpisodeRunner:
         stats.clear()
 
     def get_onehot(self, input_list):
-        # 假设你的原始列表是这样的：
         original_list = input_list
-        # 初始化一个(self.env.n_agents, self.env.m_score)维度的全零矩阵
         one_hot_matrix = np.zeros((self.env.n_agents, self.env.m_score))
-        # 遍历原始列表，将相应位置的数值设置为1，实现one-hot编码
+        # one-hot
         for i, val in enumerate(original_list):
             one_hot_matrix[i, val] = 1
-        # 将矩阵转换为列表形式，以便与其他列表拼接
         one_hot_list = one_hot_matrix.tolist()
-        # 如果需要将所有行拼接成一个新列表
         flattened_one_hot_list = [item for sublist in one_hot_list for item in sublist]
 
         return flattened_one_hot_list
