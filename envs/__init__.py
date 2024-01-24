82e86a1863be16e3f349148cba0f1db39a961e20
chenyiqun <53168756+chenyiqun@users.noreply.github.com>
2024-01-24 19:30:19 +0800

    Add files via upload

---
+++ envs/__init__.py
@@ -0,0 +1,32 @@
+from functools import partial
+import sys
+import os
+
+from .multiagentenv import MultiAgentEnv
+
+# from .starcraft import StarCraft2Env
+from .matrix_game import OneStepMatrixGame
+# from .stag_hunt import StagHunt
+from .search_engine import Search
+
+try:
+    gfootball = True
+    from .gfootball import GoogleFootballEnv
+except Exception as e:
+    gfootball = False
+    print(e)
+
+def env_fn(env, **kwargs) -> MultiAgentEnv:
+    return env(**kwargs)
+
+REGISTRY = {}
+# REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
+# REGISTRY["stag_hunt"] = partial(env_fn, env=StagHunt)
+REGISTRY["one_step_matrix_game"] = partial(env_fn, env=OneStepMatrixGame)
+REGISTRY["search_engine"] = partial(env_fn, env=Search)
+
+if gfootball:
+    REGISTRY["gfootball"] = partial(env_fn, env=GoogleFootballEnv)
+
+if sys.platform == "linux":
+    os.environ.setdefault("SC2PATH", "~/StarCraftII")
---
+++ envs/search_engine/__init__.py
@@ -0,0 +1,1 @@
+from .SearchEngine import Search
